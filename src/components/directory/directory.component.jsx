6fa72b37d09b97bc7cc23916e030afde80509331
Vignesh G <vg.thefire@gmail.com>
2022-01-24 15:46:28 +0530

    update

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+// import "./directory.styles.scss";
+
+class Directory extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+}
+
+export default Directory;
