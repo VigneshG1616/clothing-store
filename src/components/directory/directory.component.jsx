07432b5d65d5bdd71800f697273162f5e7829b7b
Vignesh G <vg.thefire@gmail.com>
2022-01-25 00:27:17 +0530

    adding menu-item and directory component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-// import "./directory.styles.scss";
-
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component";
 class Directory extends Component {
   constructor() {
     super();
@@ -40,6 +40,16 @@ class Directory extends Component {
         },
       ],
     };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
   }
 }
 
