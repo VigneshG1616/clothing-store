0c8d2e1c77216d7b7b915ba7e899cce530ac3fee
Vignesh G <vg.thefire@gmail.com>
2022-01-26 15:33:03 +0530

    added routing and withRouter capablities to the proj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
       <HomePage />
     </div>
   );
